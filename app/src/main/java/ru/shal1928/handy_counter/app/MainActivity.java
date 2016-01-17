8183cc9c51ee01d4a05f78b72fc42e249e34c019
DShalygin <DShalygin@it.ru>
2016-01-17 23:32:21 +0700

    1gg

--- app/src/main/java/ru/shal1928/handy_counter/app/MainActivity.java
+++ app/src/main/java/ru/shal1928/handy_counter/app/MainActivity.java
@@ -1,15 +1,9 @@
 package ru.shal1928.handy_counter.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.*;
 
 
 public class MainActivity extends ActionBarActivity {
